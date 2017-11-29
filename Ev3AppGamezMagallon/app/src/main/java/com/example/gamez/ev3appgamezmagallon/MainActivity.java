551a9f686a540e97a86857dc3c7ce424b4efa63a
Gamez98 <gamez.rlg@hotmail.com>
2017-11-28 22:07:37 -0600

    Mi kgadero

---
+++ Ev3AppGamezMagallon/app/src/main/java/com/example/gamez/ev3appgamezmagallon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gamez.ev3appgamezmagallon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
